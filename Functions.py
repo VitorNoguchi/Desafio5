4a79a3d1cb290f1a69d96675355339189cfff51c
Vitor Noguchi <vshojifn@gmail.com>
2020-04-15 11:52:50 -0300

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -4,6 +4,14 @@
 import MongoDB as db
 import random
 import threading
+import logging
+
+func_logger = logging.getLogger(__name__)
+func_logger.setLevel(logging.INFO)
+func_formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+funcfile_handler = logging.FileHandler('Mastermind.log')
+funcfile_handler.setFormatter(func_formatter)
+func_logger.addHandler(funcfile_handler)
 
 class mastermind:
     def __init__(self):
@@ -20,18 +28,25 @@ class mastermind:
         attempt = int(0)
         check = self.conn.check(self.conn.album, usr)
         if check == 'OK':
-            sample1 = {'Name': usr, 'Target': target_number, 'Count': attempt, 'Result': ''}
+            sample1 = {'Name': usr, 'Target': target_number, 'Count': attempt, 'Result': '', 'past_attempt': []}
             db.DB().insertion_mongo(self.conn.album, sample1)
+            func_logger.info('{one},{two},{three}'.format(one='Start', two=usr, three=target_number))
             return 'Bom Jogo'
         else:
             return 'Usuario Ja Existente'
 
     def tentativa(self, attempt_number, usr):
-        data = self.conn.find_mongo(self.conn.album, usr)
-        target_number = data['Target']
-        attempt = data['Count']
+        try:
+            data = self.conn.find_mongo(self.conn.album, usr)
+            target_number = data['Target']
+            attempt = data['Count']
+            id = data['_id']
+            list = data['past_attempt']
+        except:
+            func_logger.exception('Usuario invalido')
         if len(str(attempt_number)) == 4:
             if int(attempt) <= 10:
+                list.append(attempt_number)
                 attempt_number = str(attempt_number)
                 result = ''
                 for cont,val in enumerate(target_number):
@@ -44,21 +59,28 @@ class mastermind:
                 attempt += 1
                 if result == '1111':
                     result = "VOCE VENCEU!"
+                    func_logger.info('{one},{two},{three},Vitoria,{four}'
+                                     .format(one = id, two = usr, three = target_number, four = list))
                     mastermind.remove(self, usr)
                 elif result != '1111' and attempt == 10:
                     result = 'GAME OVER'
+                    func_logger.info('{one},{two},{three},GAMEOVER,{four}'
+                                     .format(one=id, two=usr, three=target_number, four=list))
                     mastermind.remove(self, usr)
                 else:
-                    pass
+                    func_logger.info('{one},{two},{three},Jogando,{four}'
+                                     .format(one=id, two=usr, three=target_number, four=list))
         else:
             result = 'Numero invalido'
-        updateprocess = threading.Thread(target=self.conn.update, args=[self.conn.album, usr, target_number, attempt, result])
+            func_logger.error('Numero Invalido')
+        updateprocess = threading.Thread(target=self.conn.update, args=[self.conn.album, usr,
+                                        target_number, attempt, result, list])
         updateprocess.start()
         return str('{one}    ----    {two} tentativas'.format(one = result, two =attempt))
 
     def remove(self, usr):
         self.conn.remove(self.conn.album, usr)
-        self.conn.find_mongo(self.conn.album, usr)
+        print(self.conn.find_mongo(self.conn.album, usr))
 
 if __name__ == '__main__':
     jogo = mastermind()
