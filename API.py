a56488f6baf70e42e006781b0ea6490980770427
Vitor Noguchi <vshojifn@gmail.com>
2020-04-15 11:53:14 -0300

    Update API.py

--- API.py
+++ API.py
@@ -2,25 +2,43 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, jsonify, request
-from Functions import mastermind
+import Functions
+import logging
 
-game = mastermind()
+game = Functions.mastermind()
 app = Flask(__name__)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+file_handler = logging.FileHandler('Mastermind.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 @app.route('/gera_numero', methods=['GET'])
 def foo():
-    value = game.Create_Number()
-    return jsonify(value)
+    try:
+        value = game.Create_Number()
+        return jsonify(value)
+    except:
+        logger.exception('Erro Gera Numero')
 
 @app.route('/inicia', methods=['GET'])
 def foo1():
-    usr = request.args['Name']
-    init = game.Start(usr)
+    try:
+        usr = request.args['Name']
+        init = game.Start(usr)
+        print('inicia')
+    except:
+        logger.exception('Necessario 1 Nome')
     return jsonify(init)
 
 @app.route('/tentativa/<name>/<attempt_number>', methods=['GET'])
 def foo2(name = None, attempt_number=None):
-    outcome = game.tentativa(attempt_number, name)
+    try:
+        outcome = game.tentativa(attempt_number, name)
+    except:
+        logger.exception('Inputs invalidos')
     return jsonify(outcome)
 
 @app.route('/deleta', methods = ['GET'])
